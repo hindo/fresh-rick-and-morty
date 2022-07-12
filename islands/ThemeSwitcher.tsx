e020c693ce3b0321830f1c505565a9b817d2c791
Piotr Pabich <piotr.pabich@gmail.com>
2022-07-12 17:40:59 +0200

    Improve theme switcher

--- islands/ThemeSwitcher.tsx
+++ islands/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { Fragment, h } from "preact";
 import { useEffect, useState } from "preact/hooks";
-import { tw, css, apply } from "twind/css";
+import { apply, css, tw } from "twind/css";
 import { Head } from "$fresh/runtime.ts";
 
 export default function ThemeSwitcher() {
@@ -46,8 +46,8 @@ export default function ThemeSwitcher() {
         <label
           for="theme-toggler"
           class={tw`
-              flex items-center justify-between cursor-pointer w-12 h-7 bg-gray-100
-              rounded-full relative transition-bg ring ring-gray-300
+              flex items-center justify-between cursor-pointer w-6 h-6 bg-gray-200
+              rounded-full relative transition-all overflow-hidden
             `}
         >
           <input
@@ -59,27 +59,36 @@ export default function ThemeSwitcher() {
             class={tw`peer sr-only`}
           />
           <span
-            class={tw`
-              absolute w-6 h-6 rounded-full transition bg-gray-200 active:w-6
-              ${css({
-                ".peer:checked + &": apply`right-1 bg-gray-800`,
-                ".peer:not(:checked) + &": apply`left-1`,
-                ".peer:not(:checked) + &:before": {
-                  content: '"☀️"',
-                  position: "absolute",
-                  left: "4px",
-                  width: "4px",
-                },
-                ".peer:checked + &:before": {
-                  display: "block",
-                  content: '"🌙"',
-                  position: "absolute",
-                  left: "4px",
-                  width: "4px",
-                },
-              })}
-            `}
-          ></span>
+            class={tw`absolute top-0 h-6 w-6 transition-all
+            ${
+              css({
+                ".peer:not(:checked) + &": apply`-top-6`,
+                ".peer:checked + &": apply`top-0`,
+              })
+            }
+          `}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class={tw`h-6 w-6 bg-gray-200 text-gray-800 rounded-full`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class={tw`h-6 w-6 bg-gray-800 text-gray-200 rounded-full`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
         </label>
       </div>
     </Fragment>
